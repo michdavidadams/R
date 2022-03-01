6c9e54c5d3d386303073bcaa14880273c62323b6
Michael Adams <31296356+michdavidadams@users.noreply.github.com>
2022-02-28 20:02:31 -0500

    ch 6 concepts check

--- Chapter 6/Chapter 6 Concepts Check.R
+++ Chapter 6/Chapter 6 Concepts Check.R
@@ -27,7 +27,21 @@
       y <- dunif(x, min=0.95, max=1.31)
       
   # *** Task 3 ***
-      oxyben <- 
+      oxyben <- read.csv("/Users/michaeladams/Documents/School/Probability & Data Analysis/R/Chapter 6/data sets/ex06-94oxyben.csv")
     # Method 1
-        hist(ex06.94oxyben$oxybenzene_ppt, main="Sample Distribution of Oxybenzene", xlab="ppt")  # Histogram
-      
+        hist(oxyben$oxybenzene_ppt, main="Sample Distribution of Oxybenzene", xlab="ppt")  # Histogram
+         boxplot(oxyben$oxybenzene_ppt, data=oxyben, main= "Sample Distribution of Oxybenzene")	# Box plot of oxyben$oxybenzene_pptonds
+      	stem(oxyben$oxybenzene_ppt)	# Stem plot
+     # Method 2
+     	IQR(oxyben$oxybenzene_ppt)	# Interquartile range
+     	sd(oxyben$oxybenzene_ppt)	# Standard deviation
+     # Method 3
+     	qqnorm(oxyben$oxybenzene_ppt)	# Normal quantile (or probability) plot
+     # Method 4
+     	oxyben_mean <- round(mean(oxyben$oxybenzene_ppt), 1)	# Mean
+     	oxyben_sd <- round(sd(oxyben$oxybenzene_ppt), 1)	# Standard Deviation
+		Oxyben_Interval <- c(paste("(", oxyben_mean - oxyben_sd, ",", oxyben_mean + oxyben_sd, ")", sep=""), paste("(", oxyben_mean - 2* oxyben_sd, ",", oxyben_mean + 2* oxyben_sd, ")", sep=""), paste("(", oxyben_mean - 3* oxyben_sd, ",", oxyben_mean + 3* oxyben_sd,")", sep=""))	# Intervals
+		Oxyben_Frequency <- c(sum(oxyben > oxyben_mean - oxyben_sd & oxyben < oxyben_mean + oxyben_sd), sum(oxyben > oxyben_mean - 2*oxyben_sd & oxyben < oxyben_mean + 2*oxyben_sd), sum(oxyben > oxyben_mean - 3*oxyben_sd & oxyben < oxyben_mean + 3*oxyben_sd))	# Frequencies
+		Oxyben_Proportion <- round(Oxyben_Frequency / nrow(oxyben), 2)	# Proportions
+		oxyben_backward_empirical_table <- data.frame(Oxyben_Interval, Oxyben_Frequency, Oxyben_Proportion)	# Creates table
+		print(oxyben_backward_empirical_table)	# Prints table
